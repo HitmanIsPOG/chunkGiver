dbdf4ea75eba76fa2e575e0f612cf9462f2c951d
Hitman <91709316+HitmanIsPOG@users.noreply.github.com>
2023-11-07 16:09:50 -0600

    Update ChunkGiver.java

--- src/main/java/xyz/notanipgrabber/chunkgiver/ChunkGiver.java
+++ src/main/java/xyz/notanipgrabber/chunkgiver/ChunkGiver.java
@@ -1,81 +1,87 @@
 package xyz.notanipgrabber.chunkgiver;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
-public class ChunkGiverCommand implements CommandExecutor, TabCompleter {
-
+public final class ChunkGiver extends JavaPlugin implements Listener {
+    private HashMap<String, BukkitRunnable> playerTimers = new HashMap<String, BukkitRunnable>();
     private DatabaseManager databaseManager;
 
-    public ChunkGiverCommand(DatabaseManager d) {
-        databaseManager = d;
+    @Override
+    public void onEnable() {
+        databaseManager = new DatabaseManager();
+        databaseManager.connect();
+
+        getServer().getPluginManager().registerEvents(this, this);
+        getCommand("chunk").setExecutor(new ChunkGiverCommand(databaseManager));
+        getCommand("chunk").setTabCompleter(new ChunkGiverCommand(databaseManager));
+    }
+
+    @Override
+    public void onDisable() {
+        databaseManager.disconnect();
     }
 
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-        if (args.length >= 2) {
-            if (args[1].equalsIgnoreCase("Enable")) {
-                if (args[0].equalsIgnoreCase("Sound")) {
-                    databaseManager.setPlayerSoundSetting(player.getUniqueId().toString(), true);
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        databaseManager.savePlayerData(player.getUniqueId().toString(), true, true);
+        giveChunks(player);
 
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&dClaims&7] &aEnabled &fChunk Sound!"));
-                } else if (args[0].equalsIgnoreCase("Both")) {
-                    databaseManager.setPlayerSoundSetting(player.getUniqueId().toString(), true);
-                    databaseManager.setPlayerMessageSetting(player.getUniqueId().toString(), true);
+    }
 
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&dClaims&7] &aEnabled &fChunk Message and Sound!"));
-                }  else if (args[0].equalsIgnoreCase("Message")) {
-                    databaseManager.setPlayerMessageSetting(player.getUniqueId().toString(), true);
+    public void giveChunks(Player player) {
+        BukkitRunnable timer = new BukkitRunnable() {
+            @Override
+            public void run() {
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lands admin player " + player.getName() + " give permission chunks " + (getLandChunksMax(player) + 1));
+                if (databaseManager.getPlayerSoundSetting(player.getUniqueId().toString()) && databaseManager.getPlayerMessageSetting(player.getUniqueId().toString())) {
 
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&dClaims&7] &aEnabled &fChunk Message!"));
+                    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.0f);
+                    sendActionBar(player, ChatColor.translateAlternateColorCodes('&', "&e&lClaims &fYou Have Been Given 1 Chunk!"));
+                } else if (databaseManager.getPlayerMessageSetting(player.getUniqueId().toString())) {
+                    sendActionBar(player, ChatColor.translateAlternateColorCodes('&', "&e&lClaims &fYou Have Been Given 1 Chunk!"));
+                } else if (databaseManager.getPlayerSoundSetting(player.getUniqueId().toString())) {
+                    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.0f);
+                } else {
+
                 }
-            } else if (args[1].equalsIgnoreCase("Disable")) {
-                if (args[0].equalsIgnoreCase("Sound")) {
-                    databaseManager.setPlayerSoundSetting(player.getUniqueId().toString(), false);
 
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&dClaims&7] &cDisabled &fChunk Sound!"));
-                } else if (args[0].equalsIgnoreCase("Both")) {
-                    databaseManager.setPlayerSoundSetting(player.getUniqueId().toString(), false);
-                    databaseManager.setPlayerMessageSetting(player.getUniqueId().toString(), false);
-
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&dClaims&7] &cDisabled &fChunk Message and Sound!"));
-                }  else if (args[0].equalsIgnoreCase("Message")) {
-                    databaseManager.setPlayerMessageSetting(player.getUniqueId().toString(), false);
-
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&dClaims&7] &cDisabled &fChunk Message!"));
-                }
+                giveChunks(player);
             }
+        };
 
-        }
-        }
-        return false;
+        timer.runTaskLater(this, 20L * 60L * 30L); // Run every 30 minutes (20 t    icks per second)
     }
 
-    @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        List<String> completions = new ArrayList<>();
+    public void sendActionBar(Player player, String message) {
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+    }
 
-        if (args.length == 1) { // First argument
-            completions.add("Message");
-            completions.add("Both");
-            completions.add("Sound");
-        } else if (args.length == 2) { // Second argument
-            completions.add("Enable");
-            completions.add("Disable");
+
+    public int getLandChunksMax(Player player) {
+        String placeholder = "%lands_land_chunks_max%";
+        String value = PlaceholderAPI.setPlaceholders(player, placeholder);
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            // Handle parsing error
+            return 0; // Return a default value if parsing fails
         }
-
-        return completions;
     }
 
 }
